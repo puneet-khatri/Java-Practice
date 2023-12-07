93e9f187c927400c281865eaf2737e416f3b740c
puneet-khatri <khatripuneet2000@gmail.com>
2023-12-07 13:36:17 +0530

    .

---
+++ hello.java
@@ -0,0 +1,6 @@
+class Firstclass {
+    public static void main(String[] args) {
+        System.out.print("Hello Wprld");
+        System.out.println();
+    }
+}
---
+++ ifElse.java
@@ -0,0 +1,10 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int number = 13;
+        if (number%2 == 0) {
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+    }
+}
