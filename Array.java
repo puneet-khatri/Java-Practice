3eadb68c37fdd85c5f79cf43f6312b3e7b078a93
puneet-khatri <khatripuneet2000@gmail.com>
2023-12-07 15:53:21 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+        public static void main(String[] args) {
+            int a[] = new int[5]; // Declare and initialize an array of size 5.
+            a[0]=10;
+            a[1]=20;
+            a[2]=30;
+            a[3]=40;
+            a[4]=50;
+            System.out.println("Array elements are:");
+            for (int i=0;i<a.length;i++) {
+                System.out.print(a[i]+" "); // Print all the elements in the array.
+    
+        }
+}
+}
